66d1f50d30ecdbbac56d259b169532548de99c9c
GalinaSazonova <galka.saz@yandex.ru>
2014-02-24 18:51:13 +0400

    12.02.14

---
+++ 12.02.14/1/ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public static double Factorial (int n)
+        {
+            return n <= 1 ? 1 : n * Factorial(n - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Please, enter a value");
+            int n = System.Int32.Parse(System.Console.ReadLine());
+            if (n >= 0)
+            {
+                double fact = Factorial(n);
+                System.Console.WriteLine("Factorial of n = {0}", fact);
+            }
+            else
+            {
+                System.Console.WriteLine("Wrong value");
+            }
+        }
+    }
+}
---
+++ 12.02.14/2/ConsoleApplication2/Program.cs
@@ -0,0 +1,33 @@
+﻿
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        public static double Fibbon(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibbon(n - 1) + Fibbon(n - 2);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Please, enter a number");
+            int n = System.Int32.Parse(System.Console.ReadLine());
+            if (n >= 0)
+            {
+                double fib = Fibbon(n);
+                System.Console.WriteLine("Mean of n's number is = {0}", fib);
+            }
+            else
+            {
+                System.Console.WriteLine("Wrong value");
+            }
+        }
+    }
+}
