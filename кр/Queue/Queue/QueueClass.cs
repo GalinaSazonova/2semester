0894a98029e335feeb1212934d22d537dccb1061
GalinaSazonova <galka.saz@yandex.ru>
2014-04-09 12:50:09 +0400

    queue

---
+++ кр/Queue/Queue/QueueClass.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    /// <summary>
+    /// queue on references
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class QueueClass<T>
+    {
+        private class QueueElement
+        {
+            /// <summary>
+            /// Value of Queue element.
+            /// </summary>
+            public T Value { get; set; }
+
+            public int Priority { get; set; }
+
+            /// <summary>
+            /// Reference on next element.
+            /// </summary>
+            public QueueElement Next { get; set; }
+            /// <summary>
+            /// Reference on prev element.
+            /// </summary>
+            public QueueElement Back { get; set; }
+
+            public QueueElement(T value, int priority)
+            {
+                Value = value;
+                Priority = priority;
+            }
+        }
+
+            private QueueElement head;
+            private int size;
+            private int max;
+
+            /// <summary>
+            /// add element with priority
+            /// </summary>
+            /// <param name="value"></param>
+            /// <param name="prior"></param>
+            public void Enqueue(T value, int prior)
+            {
+                QueueElement temp = head;
+                QueueElement newElement = new QueueElement(value, prior);
+                newElement.Next = temp;
+                newElement.Back = null;
+                head = newElement;
+                if (size != 0)
+                {
+                    temp.Back = head;
+                }
+                size++;
+                if (newElement.Priority > max)
+                {
+                    max = newElement.Priority;
+                }
+            }
+
+            /// <summary>
+            /// check if queue is empty
+            /// </summary>
+            /// <returns></returns>
+            public bool IsEmpty()
+            {
+                return (size == 0);
+            }
+
+            /// <summary>
+            /// count new max priority.
+            /// </summary>
+            private void NewMax()
+            {
+                if (!IsEmpty())
+                {
+                    var temp = head;
+                    max = head.Priority;
+                    while (temp.Next != null)
+                    {
+                        if (temp.Priority > max)
+                        {
+                            max = temp.Priority;
+                        }
+                        temp = temp.Next;
+                    }  
+                }
+                else
+                {
+                    max = 0;
+                }
+            }
+
+            /// <summary>
+            /// Returns element with biggest priority, which was first added
+            /// </summary>
+            /// <returns></returns>
+            public T Dequeue()
+            {
+                if (IsEmpty())
+                {
+                    throw new Exception();
+                }
+                else
+                {
+                    var temp = head;
+                    var toReturn = head;
+                    while (temp.Next != null)
+                    {
+                        if (temp.Priority == max)
+                        {
+                            toReturn = temp;
+                        }
+                        temp = temp.Next;
+                    }
+
+                    var check = toReturn.Value;
+
+                    if (toReturn == head)
+                    {
+                        head = toReturn.Next;
+                        size--;
+                        NewMax();
+                        return check;
+                    }
+                    if (toReturn.Next == null)
+                    {
+                        temp.Back.Next = null;
+                        size--;
+                        NewMax();
+                        return check;
+                    }
+                    toReturn.Back.Next = toReturn.Next;
+                    toReturn.Next.Back = toReturn.Back;
+                    size--;
+                    NewMax();
+                    return check;
+                }
+            }
+
+        /// <summary>
+        /// returns current max priority
+        /// </summary>
+        /// <returns></returns>
+        public int MaxPr()
+            {
+                return max;
+            }
+        }
+    }
---
+++ кр/Queue/QueueTest/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Queue;
+
+namespace QueueTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            queue = new QueueClass<int>();
+        }
+    
+        [TestMethod]
+        public void AddTest()
+        {
+            queue.Enqueue(5, 7);
+            Assert.IsFalse(queue.IsEmpty());
+        }
+
+        [TestMethod]
+        public void DequeueTest()
+        {
+            queue.Enqueue(5, 7);
+            queue.Enqueue(9, 8);
+            Assert.IsFalse(queue.IsEmpty());
+            Assert.AreEqual(9, queue.Dequeue());
+            Assert.IsFalse(queue.IsEmpty());
+            Assert.AreEqual(7, queue.MaxPr());
+            Assert.AreEqual(5, queue.Dequeue());
+            Assert.AreEqual(0, queue.MaxPr());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void DequeueExTest()
+        {
+            queue.Dequeue();
+        }
+        private QueueClass<int> queue;
+    }
+}
