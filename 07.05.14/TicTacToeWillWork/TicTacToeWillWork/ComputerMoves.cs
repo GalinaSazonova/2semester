7b99d52b11b13f637af63f059cc49f23663b7599
GalinaSazonova <galka.saz@yandex.ru>
2014-05-22 21:53:38 +0400

    TicTacToe Finally!!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ 07.05.14/TicTacToeWillWork/TicTacToeWillWork/ComputerMoves.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace TicTacToeWillWork
+{
+    /// <summary>
+    /// Class for computer's strategy.
+    /// </summary>
+    public class ComputerMoves
+    {
+        /// <summary>
+        /// Function, that decides, where should toe be, does not make unwinnable moves
+        /// </summary>
+        /// <param name="crossArr"></param>
+        /// <param name="allCellsArr"></param>
+        /// <param name="toeArr"></param>
+        /// <returns></returns>
+        public int PutToe(bool[] crossArr, bool[] allCellsArr, bool[] toeArr)  
+        {
+            if (Urgent(toeArr, allCellsArr) != -1)
+                return Urgent(toeArr, allCellsArr);
+            if (Urgent(crossArr, allCellsArr) != -1)
+                return Urgent(crossArr, allCellsArr);
+            if (NoObviousMoves(crossArr, allCellsArr, toeArr) != -1)
+                return NoObviousMoves(crossArr, allCellsArr, toeArr);
+            return -1;
+        }
+
+        private int Urgent(bool[] SignArr, bool[] allCellsArr)
+        {
+            int check = -1;
+            for (int i = 0; i < 3; i++)
+            {
+                check = CountPlace(SignArr, allCellsArr, i * 3, i * 3 + 1, i * 3 + 2);
+                if (check != -1)
+                    return check;
+                check = CountPlace(SignArr, allCellsArr, i % 3, i % 3 + 3, i % 3 + 6);
+                if (check != -1)
+                    return check;
+            }
+            check = CountPlace(SignArr, allCellsArr, 0, 4, 8);
+            if (check != -1)
+                return check;
+            check = CountPlace(SignArr, allCellsArr, 2, 4, 6);
+            if (check != -1)
+                return check;
+            return check;
+        }
+
+        private int CountPlace(bool[] SignArr, bool[] allCellsArr, int a, int b, int c)
+        {
+            if (SignArr[a] && SignArr[a] == SignArr[b] && !allCellsArr[c])
+                return c;
+            if (SignArr[a] && SignArr[a] == SignArr[c] && !allCellsArr[b])
+                return b;
+            if (SignArr[b] && SignArr[c] == SignArr[b] && !allCellsArr[a])
+                return a;
+            return -1;
+        }
+
+        private int NoObviousMoves(bool[] crossArr, bool[] allCellsArr, bool[] toeArr)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (!allCellsArr[i])
+                {
+                    toeArr[i] = true;
+                    allCellsArr[i] = true;
+                    int temp = Urgent(toeArr, allCellsArr);
+                    if (temp != -1)
+                    {
+                        toeArr[i] = false;
+                        allCellsArr[i] = false;
+                        return temp;
+                    }
+                    toeArr[i] = false;
+                    allCellsArr[i] = false;
+                    if (!allCellsArr[4])
+                    {
+                        return 4;
+                    }
+                    if (i % 2 == 1 && crossArr[0] && crossArr[0] == crossArr[8] && !allCellsArr[i])
+                    {
+                        return i;
+                    }
+                    if (i % 2 == 1 && crossArr[2] && crossArr[6] == crossArr[2] && !allCellsArr[i])
+                    {
+                        return i;
+                    }
+                    if (allCellsArr[4] && crossArr[4])
+                    {
+                        if (i % 2 == 0 && !allCellsArr[i])
+                        {
+                            return i;
+                        }
+                    }
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ 07.05.14/TicTacToeWillWork/TicTacToeWillWork/MainPage.xaml.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace TicTacToeWillWork
+{
+    /// <summary>
+    /// Class for TicTacToe Game.
+    /// </summary>
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            InitializeButtons();
+        }
+
+        private void InitializeNewGame(string message)
+        {
+            Label label1 = new Label();
+            label1.Content = message;
+            label1.FontSize = 60;
+            label1.HorizontalAlignment = HorizontalAlignment.Left;
+            label1.VerticalAlignment = VerticalAlignment.Top;
+            label1.Margin = new Thickness(100, 0, 0, 0);
+            this.LayoutRoot.Children.Add(label1);
+
+            Button wantToContinue = new Button();
+            wantToContinue.Content = "Play";
+            wantToContinue.FontSize = 30;
+            wantToContinue.HorizontalAlignment = HorizontalAlignment.Left;
+            wantToContinue.VerticalAlignment = VerticalAlignment.Top;
+            wantToContinue.Margin = new Thickness(130, 100, 0, 0);
+            wantToContinue.Click += new RoutedEventHandler((object sender, RoutedEventArgs e) => ButtonWantToContinueClick());
+            this.LayoutRoot.Children.Add(wantToContinue);
+        }
+
+        private void InitializeButtons()
+        {
+            computerMove = new ComputerMoves();
+            button = new Button[9];
+            markedButton = new bool[9];
+            crossButton = new bool[9];
+            toeButton = new bool[9];
+            for (int i = 0; i < 9; i++)
+            {
+                markedButton[i] = false;
+                crossButton[i] = false;
+                toeButton[i] = false;
+                button[i] = new Button();
+                button[i].Width = 167;
+                button[i].Height = 167;
+                button[i].FontSize = 30;
+                button[i].HorizontalAlignment = HorizontalAlignment.Left;
+                button[i].VerticalAlignment = VerticalAlignment.Top;
+                button[i].Margin = new Thickness((i % 3) * 167, (i / 3) * 167, 0, 0);
+                int temp = i;
+                button[i].Click += new RoutedEventHandler((object sender, RoutedEventArgs e) => ButtonClick(temp));
+                this.LayoutRoot.Children.Add(button[i]);
+            }
+        }
+
+        private void ButtonClick(int i)
+        {
+            if (!markedButton[i])
+            {
+                button[i].Content = "X";
+                markedButton[i] = true;
+                crossButton[i] = true;
+            }
+            if (ThereIsAWinner())
+            {
+                this.LayoutRoot.Children.Clear();
+                InitializeNewGame("You are winner!");
+                return;
+            }
+            int temp = computerMove.PutToe(crossButton, markedButton, toeButton);
+            if (temp != -1)
+            {
+                button[temp].Content = "O";
+                markedButton[temp] = true;
+                toeButton[temp] = true;
+            }
+            if (ThereIsAWinner())
+            {
+                this.LayoutRoot.Children.Clear();
+                InitializeNewGame("You are loser!");
+                return;
+            }
+            else
+            {
+                if (Full())
+                {
+                    this.LayoutRoot.Children.Clear();
+                    InitializeNewGame("Draw!");
+                }
+            }
+        }
+
+        private bool ThereIsAWinner()
+        {
+            if (Equals(button[0].Content, button[4].Content) && Equals(button[0].Content, button[8].Content) && PushedButtons(0, 4, 8))
+                return true;
+
+            if (Equals(button[2].Content, button[4].Content) && Equals(button[4].Content, button[6].Content) && PushedButtons(2, 4, 6))
+                return true;
+            for (int i = 0; i < 3; i++)
+            {
+                if (Equals(button[i % 3].Content, button[i % 3 + 3].Content) && Equals(button[i % 3 + 6].Content,button[i % 3].Content) && PushedButtons(i % 3, i % 3 + 3, i % 3 + 6))
+                    return true;
+                if (Equals(button[i * 3].Content, button[i * 3 + 1].Content) && Equals(button[i * 3 + 1].Content, button[i * 3 + 2].Content) && PushedButtons(i * 3, i * 3 + 1, i * 3 + 2))
+                    return true;
+            }
+            return false;
+        }
+
+        private bool PushedButtons(int a, int b, int c)
+        {
+            if (markedButton[a] && markedButton[b] && markedButton[c])
+                return true;
+            return false;
+        }
+
+        private void ButtonWantToContinueClick()
+        {
+            this.LayoutRoot.Children.Clear();
+            InitializeButtons();
+        }
+
+        private bool Full()
+        {
+            bool check = true;
+            for (int i = 0; i < 9; i++)
+            {
+                if (!markedButton[i])
+                {
+                    check = false;
+                    break;
+                }
+            }
+            return check;
+        }
+        private Button[] button;
+        private bool[] markedButton;
+        private bool[] crossButton;
+        private bool[] toeButton;
+        private ComputerMoves computerMove;
+    }
+}
